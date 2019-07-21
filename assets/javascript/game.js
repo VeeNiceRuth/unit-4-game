6b1fdf402f2dea2877513903dc6df7b3765eb430
Ruth Vazquez <rthvazquez@gmail.com>
2019-07-20 19:47:25 -0700

    updated changes by making the counter update everytime a gem is clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,36 @@
 //Displays the random number targetNumber and its a global variable
-var rdmNumber = document.getElementById("number").innerHTML = Math.floor((Math.random() * 120) + 19); 
+var rdmNumber = document.getElementById("number").innerHTML = Math.floor((Math.random() * 121) + 19); 
 var score = 0;
 //starts game
+
 function startGame() {
-      
+    var crystalNumber = Math.floor((Math.random() * 12)+ 1);  
 $("#number").text(rdmNumber);
 
-//on-click of class pink (all buttons)
+
 $(".pink").on("click", function() {
-    
-    //random values for all crystals
-    var x = Math.floor((Math.random() * 12)+ 1);
-    
-    document.getElementById("score").innerHTML = x;
-
-    console.log(x);
-
-
-
+    score = score + crystalNumber;
+    document.getElementById("score").innerHTML = score;
+    console.log(score )
     
 
-    if (score === rdmNumber) {
+    console.log(typeof(crystalNumber));
+
+ if (score === rdmNumber) {
+        console.log("win")
         document.getElementById("score").innerHTML;
     }
 
     else if (score >= rdmNumber) {
-        document.getElementById("score").innerHTML;
+        console.log("lose")
+        score = 0;
         
+        startGame();
     }
 
 });
 }
+
 startGame();
 
 

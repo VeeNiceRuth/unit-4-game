bde7737d83f6675799d99e028b22485219ad7b97
Ruth Vazquez <rthvazquez@gmail.com>
2019-07-20 20:35:19 -0700

    counter works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
 //Displays the random number targetNumber and its a global variable
 var rdmNumber = document.getElementById("number").innerHTML = Math.floor((Math.random() * 121) + 19); 
 var score = 0;
-//starts game
+var winCounter = 0;
+var loseCounter = 0;
+
 
 function startGame() {
+    rdmNumber;
     var crystalNumber = Math.floor((Math.random() * 12)+ 1);  
 $("#number").text(rdmNumber);
 
@@ -11,20 +14,23 @@ $("#number").text(rdmNumber);
 $(".pink").on("click", function() {
     score = score + crystalNumber;
     document.getElementById("score").innerHTML = score;
-    console.log(score )
+    console.log(score);
     
 
     console.log(typeof(crystalNumber));
 
  if (score === rdmNumber) {
-        console.log("win")
-        document.getElementById("score").innerHTML;
+     console.log("win!")
+        winCounter++;
+        document.getElementById("win").innerHTML = winCounter;
+        startGame();
     }
 
     else if (score >= rdmNumber) {
-        console.log("lose")
+        console.log("lose!");
+        loseCounter++;
+        document.getElementById("losses").innerHTML = loseCounter;
         score = 0;
-        
         startGame();
     }
 
